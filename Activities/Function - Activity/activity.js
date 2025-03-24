465b4f219560075f40aed13447cff139f7e6880a
obalobaoaj <ajobalobao09@gmail.com>
2025-03-24 11:26:39 +0800

    activities

---
+++ Activities/Function - Activity/activity.js
@@ -0,0 +1,14 @@
+function whatShallIWear (temp){
+    if (temp < 60){
+        console.log("Wear a Jacket");
+    }
+    else if (temp < 70){
+        console.log("Where a Sweater")
+    }
+    else{ console.log("Wear t-shirt");}
+
+   
+}
+whatShallIWear(50);
+whatShallIWear (60);
+whatShallIWear(80)
